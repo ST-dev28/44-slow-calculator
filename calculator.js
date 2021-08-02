f434620e614ddf0f856fb96e684bdaf4cf2f97ff
SandraT <sndrbltr@gmail.com>
2021-08-02 17:24:40 +0000

    Solution

--- calculator.js
+++ calculator.js
@@ -1,4 +1,11 @@
-async function calculator() {
+async function calculator(a, b) {
+    return new Promise((resolve, reject) => {
+        if (typeof a === 'number' && typeof b === 'number') {
+            resolve(a + b);
+        } else {
+            reject('No sum');
+        }
+    })
 }
 
 module.exports = calculator;
